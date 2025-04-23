09c82abcafacdd0e559a44d59c7d9339c6e77488
aansh06 <aansh0611@gmail.com>
2025-04-24 01:20:53 +0530

    get-post-patient-controller

---
+++ Patient-Service/src/main/java/com/patient/Patient_Service/controller/PatientController.java
@@ -0,0 +1,43 @@
+package com.patient.Patient_Service.controller;
+
+import com.patient.Patient_Service.dto.PatientRequestDTO;
+import com.patient.Patient_Service.dto.PatientResponseDTO;
+import com.patient.Patient_Service.service.PatientService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+//@Tag(name = "Patient", description = "API for managing Patients")
+public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+//    @Operation(summary = "Get Patients")
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+
+    @PostMapping
+//    @Operation(summary = "Create a new Patient")
+    public ResponseEntity<PatientResponseDTO> createPatient(
+            @Valid
+            @RequestBody PatientRequestDTO patientRequestDTO) {
+
+        PatientResponseDTO patientResponseDTO = patientService.createPatient(
+                patientRequestDTO);
+
+        return ResponseEntity.ok().body(patientResponseDTO);
+    }
+
+}
---
+++ Patient-Service/src/main/java/com/patient/Patient_Service/service/PatientService.java
@@ -0,0 +1,45 @@
+package com.patient.Patient_Service.service;
+
+import com.patient.Patient_Service.dto.PatientRequestDTO;
+import com.patient.Patient_Service.dto.PatientResponseDTO;
+import com.patient.Patient_Service.mapper.PatientMapper;
+import com.patient.Patient_Service.model.Patient;
+import com.patient.Patient_Service.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    private PatientRepository patientRepository;
+
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients() {
+        List<Patient> patients = patientRepository.findAll();
+
+        return patients.stream().map(PatientMapper::toDTO).toList();
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+//        if (patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
+//            throw new EmailAlreadyExistsException(
+//                    "A patient with this email " + "already exists"
+//                            + patientRequestDTO.getEmail());
+//        }
+
+        Patient newPatient = patientRepository.save(
+                PatientMapper.toModel(patientRequestDTO));
+
+//        billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(),
+//                newPatient.getName(), newPatient.getEmail());
+//
+//        kafkaProducer.sendEvent(newPatient);
+
+        return PatientMapper.toDTO(newPatient);
+    }
+}
